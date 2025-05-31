7a914dd353d6ca9587a3b063eef65b8e577f5417
Yeskkimseul <yeslkim2@gamil.com>
2025-05-31 10:51:35 +0900

    update

--- toybox/js/main.js
+++ toybox/js/main.js
@@ -1,12 +1,12 @@
 $(function () {
 
-  $('header .bottom .icon .ham').click(function(){
+  $('header .bottom .icon .ham').click(function () {
     $('header .bottom .icon .ham i').toggleClass('fa-xmark').toggleClass('fa-bars');
     $('.side').fadeToggle();
     $('.bg').addClass('on');
   });
 
-  $('.bg').click(function(){
+  $('.bg').click(function () {
     $('.side').fadeOut();
     $('.bg').removeClass('on');
     $('header .bottom .icon .ham i').addClass('fa-bars').removeClass('fa-xmark');
@@ -15,11 +15,11 @@ $(function () {
   /* 메인 슬라이드 */
   let mainSwiper = new Swiper(".main_slide", {
     loop: true,
-    /*     autoplay: {
-          delay: 3500,
-          disableOnInteraction: false,
-          pauseOnMouseEnter: true, 
-        }, */
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+    },
     effect: "fade",
     fadeEffect: {
       crossFade: true,
@@ -78,12 +78,12 @@ $(function () {
     }
   };
 
-/*   document.querySelector(".topbtn").onclick = function () {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth"
-    });
-  }; */
+  /*   document.querySelector(".topbtn").onclick = function () {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    }; */
 
   /* event slide */
 
@@ -98,6 +98,20 @@ $(function () {
       nextEl: ".pr_next_stop .fa-chevron-right",
       prevEl: ".pr_next_stop .fa-chevron-left",
     },
-  });
 
-});//ready end
+    // $('.autoplay i').click(function () {
+    //   if (chk == 0) {
+    //     //정지해야 하는 상황
+    //     mainSwiper.autoplay.stop();
+    //     chk++;
+    //   } else {
+    //     //플레이 해야 하는 상황
+    //     mainSwiper.autoplay.start();
+    //     chk = 0;
+    //   }
+    //   $(this).toggleClass('fa-pause').toggleClass('fa-play');
+    });
+
+
+
+  });//ready end
